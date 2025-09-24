5a73fe015dc910b52421d88a3073890b2f941816
SmackSnackz <Rjohnson801915@gmail.com>
2025-09-24 15:41:38 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = { 
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+};
 module.exports = nextConfig;
